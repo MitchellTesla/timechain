0a214835c2cbadd3a4b9ff4efd2608de8c6f2688
J.Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-04 06:25:19 -0500

    Update docs/Bitcoin-Core/go-bitcoin-core-rpc/examples

---
+++ docs/Bitcoin-Core/go-bitcoin-core-rpc/examples/main.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2014-2017 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+
+	rpcclient "github.com/stevenroose/go-bitcoin-core-rpc"
+)
+
+func main() {
+	// Connect to local bitcoin core RPC server using HTTP POST mode.
+	connCfg := &rpcclient.ConnConfig{
+		Host: "localhost:8332",
+		User: "yourrpcuser",
+		Pass: "yourrpcpass",
+	}
+	// Notice the notification parameter is nil since notifications are
+	// not supported in HTTP POST mode.
+	client, err := rpcclient.New(connCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Shutdown()
+
+	// Get the current block count.
+	blockCount, err := client.GetBlockCount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Block count: %d", blockCount)
+}
